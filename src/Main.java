d45668117653772dd79d7f02f412a6e566a42c8c
이하영 <digldud32@naver.com>
2025-06-04 17:48:44 +0900

    third commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 12;
-        int b = 4;
+        int number = 2;
 
-        System.out.println("a == b : " + (a == b));
-        System.out.println("a != b : " + (a !=b ));
-
-        System.out.println("a > b : " + (a > b));
-        System.out.println("a < b : " + (a < b));
-        System.out.println("a >= 10 : " + (a >= 10));
-        System.out.println("b <= 5 : " + (b <= 5 ));
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(number + " x " + i + " = " + (number * 1));
+        }
     }
 }
